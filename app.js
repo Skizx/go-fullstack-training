8e95b78fc5e42e62e732e6444a0f502dd3529bde
Kevin <kevin.le.milanais@hotmail.fr>
2022-10-18 12:22:31 +0200

    Enregistrement et récupération des données via MONGODB & Mongoose

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const express = require('express');
 // Je crée mon application express
 const app = express();
 
+// J'importe Mongoose
+const mongoose = require('mongoose');
+const Things = require('./models/Things');
+
+// J'importe le schéma des choses
+const Thing = require('./models/Things')
+
+// Je connecte mon API à ma base de données
+mongoose.connect('mongodb+srv://Skizx:sarcelles95@cluster0.lgvafq7.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 // ------------- MIDDLEWARES ------------------------
 /*app.use((req, res, next) => {
     console.log('requête reçue !');
@@ -41,17 +55,28 @@ app.use((req, res, next) => {
     next();
 })
 
-//
+// Logique de ma route POST
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message : 'Objet crée !'
-    });
+    delete req.body._id;
+    const thing = new Things({
+        ...req.body
+    })
+    thing.save()
+    .then(() => res.status(201).json({ message: 'Object enregistré !'}))
+    .catch( error => res.status(400).json ({ error }));
+});
+
+// Intercèpte uniquement les requêtes GET a cette endpoint
+// J'utilise les deux points ':' pour rendre la route accèssible en tant que paramètre
+app.get('/api/stuff/:id', (req, res, next) => {
+    Things.findOne({ _id: req.params.id})
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error}));
 })
 
 // Intercèpte uniquement les requêtes GET a cette endpoint
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
+   /* const stuff = [
         {
             _id : 'oeihfzer',
             title : 'Mon premier objet',
@@ -69,7 +94,12 @@ app.get('/api/stuff', (req, res, next) => {
             userId : 'qsomihvquoisdeu'
         },
     ];
-    res.status(200).json(stuff);
+    res.status(200).json(stuff);*/
+
+    // J'utilise la méthode find() afin de retourner un tableau contenant tous les Things dans notre base de données
+    Things.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error}));
 });
 // J'exporte cette application pour y accèder depuis les autres fichiers du projet (server node)
 module.exports = app;
---
+++ models/Things.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// Je crée un schéma de données qui contient les champs souhaités pour chaque Thing, indique leur type ainsi que leur caractère (obligatoire ou non). Pour cela, on utilise la méthode Schema mise à disposition par Mongoose. Pas besoin de mettre un champ pour l'Id puisqu'il est automatiquement généré par Mongoose
+const thingSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true},
+    imageUrl: { type: String, required: true},
+    userId: { type: String, required: true},
+    price: { type: Number, required: true},
+});
+
+// J'exporte le schéma en tant que modèle mongoose appelé "Thing" le randant disponible pour l'application Express
+module.exports = mongoose.model('Thing', thingSchema);
